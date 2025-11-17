3f93c9f35f787e51349d604a13240df73b7fc11a
AyushiVispute <ayushivispute5@gmail.com>
2025-11-17 22:51:37 +0530

    Solved Array qustions

---
+++ 9)Day_09_Array_Part1/2)Output_&_Input.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	
+	int n;
+	cout<<"Enter length of array:";
+	cin>>n;
+	
+	int arr[n];
+	
+	for(int i =0;i<n;i++){ //input
+		cin>>arr[i];
+	}
+	for(int i=0;i<n;i++){//output
+		cout<<arr[i]<<",";
+	}
+	cout<<endl;
+	return 0;
+	
+}
+output:
+Enter length of array:5
+1 2 3 4 5
+1,2,3,4,5,
---
+++ 9)Day_09_Array_Part1/3)finding_largest_value.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	
+	int  arr[]={5,4,3,9,12};
+	int n=sizeof(arr)/sizeof(int);
+	
+	int max=arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]>max){
+			max=arr[i];
+		}
+	}
+	cout<<"max="<<max<<endl;
+	return 0;
+	
+}
+//output:12
