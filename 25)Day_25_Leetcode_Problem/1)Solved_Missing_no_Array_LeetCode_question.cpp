0a038c57afe3b32d37c78f7761bd94ae8e2c248a
AyushiVispute <ayushivispute5@gmail.com>
2026-01-22 22:53:31 +0530

    solved Missing Array Question

---
+++ 25)Day_25_Leetcode_Problem/1)Solved_Missing_no_Array_LeetCode_question.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int range=nums.size();
+
+        int actualSum=(range*(range+1))/2;
+        int currentSum=0;
+
+        for(int i=0;i<nums.size();i++){
+            currentSum=currentSum+nums[i];
+        }
+        int ans=actualSum-currentSum;
+        return ans;
+
+        
+    }
+};
