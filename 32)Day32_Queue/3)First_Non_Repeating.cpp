bf357f975fa474a7b78366bf20584ddcff3d565e
AyushiVispute <ayushivispute5@gmail.com>
2026-02-21 11:04:33 +0530

    solving queue based questio

---
+++ 32)Day32_Queue/3)First_Non_Repeating.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        queue<int>q;
+        int freq[26]={0};
+
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+            q.push(i);
+            
+
+            while(!q.empty() && freq[s[q.front()]-'a']>1){
+                q.pop();
+            }
+        }
+            if(q.empty()) return -1;
+            return q.front();
+        
+        
+    }
+};
